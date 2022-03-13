0595167134d27c7deb5a8bfdd3e07c4c1427e833
paullee714 <paul_lee@kakao.com>
2022-03-13 12:44:17 +0900

    fix: Update Javasciript React to Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <h4>hello react typescript</h4>
+      <h4>hello react</h4>
     </div>
   );
 }
